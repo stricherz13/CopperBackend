bd248e2d232c25163db2371412586a66924636cf
Brad Stricherz <Brad.Stricherz@gmail.com>
2024-06-23 18:50:58 -0500

    MVP for testing 3

--- CopperBackend/settings.py
+++ CopperBackend/settings.py
@@ -13,7 +13,7 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 
-# Load environment variables from .env file
+# Load environment variables from ..env file
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
