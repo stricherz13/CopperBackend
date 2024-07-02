28fe2c9650941f2f2acfe60bd62c547aa87d5445
Brad Stricherz <Brad.Stricherz@gmail.com>
2024-07-02 16:35:14 -0500

    Added endpoint for heatmap.

--- api/test_api.py
+++ api/test_api.py
@@ -1,12 +1,17 @@
+# myapp/test_api.py
+
 from django.test import TestCase, Client
 from django.urls import reverse
 from api.models import SpeedRecord
+import json
 
 
 class SpeedInfoAPITest(TestCase):
     def setUp(self):
         self.client = Client()
-        self.url = reverse('api:speed_info')
+        self.speed_limit_url = reverse('speed-limit')
+        self.speed_info_url = reverse('speed-info')
+        self.speed_heatmap_url = reverse('speed-heatmap')
 
     def test_speed_info_post_valid(self):
         payload = {
@@ -14,7 +19,7 @@ class SpeedInfoAPITest(TestCase):
             "lon": 13.4050,
             "current_speed": 50.0
         }
-        response = self.client.post(self.url, payload, content_type='application/json')
+        response = self.client.post(self.speed_info_url, data=json.dumps(payload), content_type='application/json')
         self.assertEqual(response.status_code, 200)
         self.assertIn('latitude', response.json())
         self.assertIn('longitude', response.json())
@@ -28,7 +33,7 @@ class SpeedInfoAPITest(TestCase):
             "lon": 13.4050,
             "current_speed": 50.0
         }
-        response = self.client.post(self.url, payload, content_type='application/json')
+        response = self.client.post(self.speed_info_url, data=json.dumps(payload), content_type='application/json')
         self.assertEqual(response.status_code, 422)
 
     def test_speed_record_creation(self):
@@ -37,10 +42,27 @@ class SpeedInfoAPITest(TestCase):
             "lon": 13.4050,
             "current_speed": 50.0
         }
-        response = self.client.post(self.url, payload, content_type='application/json')
+        response = self.client.post(self.speed_info_url, data=json.dumps(payload), content_type='application/json')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(SpeedRecord.objects.count(), 1)
         speed_record = SpeedRecord.objects.first()
         self.assertEqual(speed_record.latitude, payload['lat'])
         self.assertEqual(speed_record.longitude, payload['lon'])
         self.assertEqual(speed_record.current_speed, payload['current_speed'])
+
+    def test_speed_heatmap_get(self):
+        # Create some SpeedRecord instances
+        SpeedRecord.objects.create(latitude=52.5200, longitude=13.4050, current_speed=50.0, road_speed_limit=30.0,
+                                   speed_difference=20.0)
+        SpeedRecord.objects.create(latitude=52.5201, longitude=13.4051, current_speed=40.0, road_speed_limit=30.0,
+                                   speed_difference=10.0)
+
+        response = self.client.get(self.speed_heatmap_url)
+        self.assertEqual(response.status_code, 200)
+        data = response.json()
+        self.assertEqual(data['type'], 'FeatureCollection')
+        self.assertEqual(len(data['features']), 1)
+        feature = data['features'][0]
+        self.assertEqual(feature['geometry']['type'], 'LineString')
+        self.assertEqual(len(feature['geometry']['coordinates']), 2)
+        self.assertEqual(len(feature['properties']['speed_differences']), 2)
